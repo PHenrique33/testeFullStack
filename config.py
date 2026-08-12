0e04d8c33cfb0ed2a339af51332c6d299f10ce61
Pedro Faculdade <pedrohenrique10092007@gmail.com>
2026-08-12 16:51:37 -0300

    minor changes

--- config.py
+++
@@ -1,6 +0,0 @@
-import os # usado para conversar com o sistema operacional, permite o uso do getenv
-from dotenv import load_dotenv #usado para ler a pasta .env onde está a chave e o modelo da IA
-
-load_dotenv() #habilitar o .env para esconder a chave da IA
-
-gemini_key = Key foda #Chave da IA
